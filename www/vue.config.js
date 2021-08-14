1ad9379ece6acd65a39a78835ba3e6c20b6cb98a
daanh432 <daan@daanhendriks.nl>
2021-08-14 17:38:54 +0200

    Bump icon cache

--- www/vue.config.js
+++ www/vue.config.js
@@ -7,14 +7,14 @@ module.exports = {
         msTileColor: '#111827',
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
-        assetsVersion: '2',
+        assetsVersion: '3',
 
         iconPaths: {
-            favicon32: 'img/icons_2/favicon-32x32.png',
-            favicon16: 'img/icons_2/favicon-16x16.png',
-            appleTouchIcon: 'img/icons_2/apple-touch-icon-152x152.png',
-            maskIcon: 'img/icons_2/safari-pinned-tab.svg',
-            msTileImage: 'img/icons_2/msapplication-icon-144x144.png'
+            favicon32: 'img/icons_3/favicon-32x32.png',
+            favicon16: 'img/icons_3/favicon-16x16.png',
+            appleTouchIcon: 'img/icons_3/apple-touch-icon-152x152.png',
+            maskIcon: 'img/icons_3/safari-pinned-tab.svg',
+            msTileImage: 'img/icons_3/msapplication-icon-144x144.png'
         },
 
         // configure the workbox plugin
