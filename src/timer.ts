b6a4485f2b2f38893de1ecf3c4bc9db0d55219da
daanh432 <daan@daanhendriks.nl>
2024-04-20 16:27:11 +0200

    Transaction setup

--- src/timer.ts
+++ src/timer.ts
@@ -1,21 +1,26 @@
+import { sequelize } from '.';
 import { ImapListener } from './datasources/Imap';
 import { Pakbon } from './models/Pakbon';
-import { Product } from "./models/Product";
 
 export const startTimer = () => {
     const imapListener = new ImapListener(process.env.IMAP_USER, process.env.IMAP_PASSWORD, process.env.IMAP_HOST, process.env.IMAP_PORT, process.env.IMAP_TLS === "true");
-    const intervalInMinutes = 5;
+    const intervalInMinutes = 1;
 
     const searchPakbon = async () => {
         imapListener.searchMessages(async (pakbon: Pakbon) => {
             let searchPakbon = await Pakbon.findOne({ where: { order_number: pakbon.getOrderNumber() } });
             if (searchPakbon != null) return;
 
-            const savedPakbon = await pakbon.save();
-            pakbon.products.forEach(async (product) => {
-                product.package_slip_id = savedPakbon.id;
+            await sequelize.transaction(async t => {
+                const savedPakbon = await pakbon.save({ transaction: t });
+                const products = await Promise.all(pakbon.getProducts()
+                    .map(product => {
+                        product.package_slip_id = savedPakbon.id;
+                        product.package_slip = savedPakbon;
+                        return product.save({ transaction: t });
+                    }));
+                return { pakbon: savedPakbon, products };
             });
-            await Product.bulkCreate(pakbon.getProducts());
         });
     }
 
